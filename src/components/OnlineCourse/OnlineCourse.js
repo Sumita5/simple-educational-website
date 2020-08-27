4c661cb0ec456429eb1c544d7aa8a2cf34ad4a52
sajibchandraroy <sajibchandraroy@gmail.com>
2020-08-27 22:44:17 +0600

    finally finish

--- src/components/OnlineCourse/OnlineCourse.js
+++ src/components/OnlineCourse/OnlineCourse.js
@@ -1,18 +1,22 @@
 import React, { useState} from 'react';
 import fakeData from '../fakeData/TotalData';
+import './OnlineCourse.css'
 import Courses from '../Courses/Courses';
 import Cart from '../Cart/Cart';
-// import { Alert } from 'bootstrap';
+
 // fake data call
 const OnlineCourse = () => {
     const data = fakeData;
     const [courses, setCourses] = useState(data); 
     const [cart, setCart] = useState([]);
 
-    const addToEnroll = (course) => { //Add course to cart function
+    //Add course to cart function
+
+    const addToEnroll = (course) => { 
         const totalCartItem = [...cart, course];
         setCart(totalCartItem);
     }
+
     // get total price
     const totalPrice = cart.reduce((total, item) => total + item.price, 0); 
 
@@ -21,13 +25,13 @@ const OnlineCourse = () => {
             <h1 className="text-center mt-3 text-success">Welcome To Online Courses</h1>
             <div className="row">
                 <div className="col-md-3 cart">
-                    <h2 className="text-center">Total Cart Item : {cart.length} </h2>
+                    <h2 className="text-center">Total Cart Item : {cart.length}</h2>
                     <ul className="list-group">
                         {
                             cart.map(item => <Cart key={item.id * Math.random()} item={item} />)
                         }
                     </ul>
-                    Checkout <span className="badge badge-light">$ {totalPrice}</span>
+                    <h6>Checkout<span className="badge badge-light">$ {totalPrice}</span></h6>
                 </div>
                 <div className="col-md-9 row border-left course-item">
                     {
